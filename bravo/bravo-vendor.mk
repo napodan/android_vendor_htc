7c36f48ee44bd4699803c200f6f5027156abe4e2
napodan <david.guenet@gmail.com>
2013-08-08 08:17:26 +0200

    Remove conflict on wallpaper

--- bravo/bravo-vendor.mk
+++ bravo/bravo-vendor.mk
@@ -22,10 +22,6 @@ PRODUCT_PACKAGES := \
     VisualizationWallpapers \
     librs_jni
 
-# Publish that we support the live wallpaper feature.
-PRODUCT_COPY_FILES := \
-    packages/wallpapers/LivePicker/android.software.live_wallpaper.xml:/system/etc/permissions/android.software.live_wallpaper.xml
-
 # Pick up overlay for features that depend on non-open-source files
 DEVICE_PACKAGE_OVERLAYS := vendor/htc/bravo/overlay
 
